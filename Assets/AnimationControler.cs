a6905c716930d9519503488c5158349d5d308469
I-Bearman <i-medvedev92@yandex.ru>
2022-04-27 18:02:20 +0700

    Changes 26.04

---
+++ Assets/AnimationControler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+public class AnimationControler : MonoBehaviour
+{
+    private bool CanDustDiffusion;
+    private PlayableDirector timeLine;
+    [SerializeField] private Text StartCutSceneText;
+    [SerializeField] private GameObject MisteriusDust, House;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLine = GetComponent<PlayableDirector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(StartCutSceneText);
+            timeLine.Play();
+        }
+
+        if(MisteriusDust && CanDustDiffusion)
+        {
+            MisteriusDust.transform.localScale *= 1.01f;
+        }
+    }
+
+    public void MistDustEmergence()
+    {
+        MisteriusDust.SetActive(true);
+        StartCoroutine(WaitSomeSeconds(2));
+
+        CanDustDiffusion = true;
+        Rigidbody DustBody = MisteriusDust.GetComponent<Rigidbody>();
+        DustBody.AddForce(Vector3.up*500, ForceMode.Acceleration);
+    }
+
+    public void DestroyMistDust()
+    {
+        Destroy(MisteriusDust);
+    }
+
+    private IEnumerator WaitSomeSeconds(float count)
+    {
+        yield return new WaitForSeconds(count);
+    }
+    
+}
