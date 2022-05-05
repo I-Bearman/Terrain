d41dbbfea8d17665440b225ef50d99dac739f907
I-Bearman <i-medvedev92@yandex.ru>
2022-05-05 11:26:08 +0700

    Final Changes of Cinemachine HomeWork

---
+++ Assets/CinemachineTriggerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class CinemachineTriggerController : MonoBehaviour
+{
+    [SerializeField] private Collider mainHeroCollider;
+    [SerializeField] private PlayableDirector timeLine;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == mainHeroCollider)
+        {
+            timeLine.Play();
+            Destroy(gameObject);
+        }
+    }
+}
